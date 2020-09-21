24159abb36472b5b25a29495a47d8f642e9567e4
rozap750 <rozap76750@gmail.com>
2020-09-21 14:16:33 +0700

    mengatur fitur sebelum login

--- app/Views/navbar.php
+++ app/Views/navbar.php
@@ -5,39 +5,29 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
+        <?php
+        $session = session();
+        if ($session->get('isLoggedIn')) : ?>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        <?php endif; ?>
         <div class="my-2 mylg-0">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a href="<?= site_url('auth/login'); ?>" class="btn btn-dark">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a href="<?= site_url('auth/register'); ?>" class="btn btn-dark">Register</a>
-                </li>
-                <li class="nav-item">
-                    <a href="<?= site_url('auth/logout'); ?>" class="btn btn-dark">Logout</a>
-                </li>
+                <?php if ($session->get('isLoggedIn')) : ?>
+                    <li class="nav-item">
+                        <a href="<?= site_url('auth/logout'); ?>" class="btn btn-dark">Logout</a>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item">
+                        <a href="<?= site_url('auth/login'); ?>" class="btn btn-dark">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= site_url('auth/register'); ?>" class="btn btn-dark">Register</a>
+                    </li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
